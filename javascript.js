4a4c77cd57ad8f6b85755101260b5e6dd0e51f50
m2tg <monishrvps2008@gmail.com>
2023-07-18 02:15:24 +0530

    Implemented parseAndEvaluate function
    The parseAndEvaluate splits input string at each operator and performs the operation according to DMAS rule and outputs result into div.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,59 @@
-const inputDiv = document.querySelector('expression');
+const inputDiv = document.querySelector('.expression');
 const buttons = document.querySelectorAll('.key');
 
-const expression = '';
+let expression = '';
+
+function parseAndEvaluate() {
+    parsedExpression = expression.split(/([-+*\/])/g);
+    //console.log(parsedExpression);
+    while (parsedExpression.length !== 1) {
+        if (parsedExpression.includes('/')) {
+            parsedExpression.forEach( (element, index) => {
+                if(element == '/') {
+                    parsedExpression.splice(index-1, 3, parseInt(parsedExpression[index-1])/parseInt(parsedExpression[index+1]));
+                }
+            })
+        }
+        else if (parsedExpression.includes('*')) {
+            parsedExpression.forEach( (element, index) => {
+                if(element == '*') {
+                    parsedExpression.splice(index-1, 3, parseInt(parsedExpression[index-1])*parseInt(parsedExpression[index+1]));
+                }
+            })
+        }
+        else if (parsedExpression.includes('+')) {
+            parsedExpression.forEach( (element, index) => {
+                if(element == '+') {
+                    parsedExpression.splice(index-1, 3, parseInt(parsedExpression[index-1])+parseInt(parsedExpression[index+1]));
+                }
+            })
+        }
+        else if (parsedExpression.includes('-')) {
+            parsedExpression.forEach( (element, index) => {
+                if(element == '-') {
+                    parsedExpression.splice(index-1, 3, parseInt(parsedExpression[index-1])-parseInt(parsedExpression[index+1]));
+                }
+            })
+        }
+        //console.log(parsedExpression);
+    }
+    inputDiv.textContent = parsedExpression;
+}
 
 function compute (e) {
-
+    const dataValue = this.getAttribute('data-value');
+    if (dataValue === '='){
+        parseAndEvaluate();
+    }
+    else if (dataValue === 'C') {
+        inputDiv.textContent = '';
+        expression = '';
+    }
+    else {
+        expression += dataValue;
+        
+        inputDiv.textContent = expression;
+    }
 }
 
 buttons.forEach((button) => {
